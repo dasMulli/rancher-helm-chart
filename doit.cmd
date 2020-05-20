460d3845c43496b273306d2803f71774b30e98c0
Markus Diepold <markus@diepold.net>
2020-05-20 17:09:59 +0200

    grafana-helm

--- doit.cmd
+++ doit.cmd
@@ -1,4 +1,7 @@
 helm package panda .\panda\
+helm package infra .\infra\
+helm package portal .\portal\
+
 helm repo index --url https://vandenbergen.github.io/rancher-helm-chart/ --merge index.yaml .
 git add -A
 git commit -am "grafana-helm"
