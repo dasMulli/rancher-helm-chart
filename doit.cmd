9f56866d06428346d1d6cd1600d179e8401811cc
Markus Diepold <markus@diepold.net>
2020-05-26 15:36:51 +0200

    grafana-helm_2020-05-26_15_36_51

--- doit.cmd
+++ doit.cmd
@@ -5,6 +5,7 @@ helm package portal .\portal\
 for /f "skip=5 eol=: delims=" %%F in ('dir /b /o-d panda*.tgz') do @del "%%F"
 for /f "skip=5 eol=: delims=" %%F in ('dir /b /o-d infra*.tgz') do @del "%%F"
 for /f "skip=5 eol=: delims=" %%F in ('dir /b /o-d portal*.tgz') do @del "%%F"
+del index.yaml
 
 helm repo index --url https://vandenbergen.github.io/rancher-helm-chart/ --merge index.yaml .
 git add -A
