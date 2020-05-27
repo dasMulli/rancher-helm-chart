cf11a144ce6e7bcafbcf0ebfe292838a75953c4e
Markus Diepold <markus@diepold.net>
2020-05-27 08:58:12 +0200

    grafana-helm_2020-05-27_08_58_12

--- doit.cmd
+++ doit.cmd
@@ -9,6 +9,8 @@ del index.yaml
 
 helm repo index --url https://vandenbergen.github.io/rancher-helm-chart/ --merge index.yaml .
 git add -A
-set mydate=grafana-helm_%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
+set NantTime=%time: =0%
+set mydate=grafana-helm_%date:~6,4%-%date:~3,2%-%date:~0,2%_%NantTime:~0,2%_%NantTime:~3,2%_%NantTime:~6,2%
+echo %mydate%
 git commit -am %mydate%
 git push
